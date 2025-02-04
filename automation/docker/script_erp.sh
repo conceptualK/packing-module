66bd52842570332524d0cf88bf03e9b3f2396222
Wattana Kawang <conceptual.watka@gmail.com>
2025-02-04 07:54:42 +0700

    LF

--- automation/docker/script_erp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dotnet /app/control_erp/Control_ERP.dll
-echo "Control_ERP Successfully $(TZ='Asia/Bangkok' date +'%F %T')" >> /var/log/cron_erp.log
-
--- automation/docker/script_part.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dotnet /app/update_part/Control_Part.dll
-echo "Control_Part Successfully $(TZ='Asia/Bangkok' date +'%F %T')" >> /var/log/cron_part.log
-
